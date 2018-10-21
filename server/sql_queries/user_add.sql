984b69215d77d7e443a0bc8eba7477c9afbf2af4
Ilya Protasov <ilya.protasov@omega-r.com>
2018-10-21 21:22:49 +0300

    wtf2

--- server/sql_queries/user_add.sql
+++ server/sql_queries/user_add.sql
@@ -1,1 +1,4 @@
 insert into user (id_user, adress, date_of_birth, `first_name`, last_name, login, password, sex, status) values (1, 'Street', '12.12.12', 'User', 'User', 'user', '123', 'unknown', 'what');
+
+
+SELECT id_user FROM user WHERE login = 'user' AND password = '123';
