df9f7f70d5e11b37122f6819dab821640802514c
Ilya Protasov <ilya.protasov@omega-r.com>
2018-10-30 22:31:53 +0300

    repaire bugs

--- server/requests/requests.js
+++ server/requests/requests.js
@@ -1,27 +1,3 @@
-exports.addUser = function(database, data, next) {
-	return new Promise(async (resolve, reject) => {
-		let sql = `SELECT * FROM user WHERE email = '${data.email}'`;
-		database.query(sql, function (err, result) {
-			if (err) {
-				console.log(err);
-			}
-
-			if (result.length !== 0) {
-				return reject(false);
-			}
-		});
-
-		sql = `INSERT INTO user VALUES (null, '', '', 1, '', '${data.password}', '${data.email}', '')`;
-		database.query(sql, function (err) {
-			if (err) {
-				console.log(err);
-			}
-
-			return resolve(true);
-		});
-	});
-};
-
 exports.loginUser = function (database, data, next) {
 	return new Promise(async (resolve, reject) => {
 		let response = { status: "unknown" };
@@ -52,19 +28,19 @@ exports.registerUser = function (database, data, next) {
 		database.query(sql, (err, result) => {
 			if (err) {
 				console.log("err in register user");
-				reject("err");
+				reject(response);
 			} else {
 				if (result.length !== 0) {
-					resolve(JSON.stringify(response));
+					reject(response);
 				} else {
 					let addedSql = `insert into user (login, password, first_name, last_name, status, date_of_birth, address, sex) values ('${data.login}', '${data.password}', '${data.firstName}', '${data.lastName}', '${data.status}', '${data.dateOfBirth}', '${data.address}', '${data.sex}')`;
 					database.query(addedSql, (err, newResult) => {
 						if (err) {
 							console.log("err in register user");
-							reject("err");
+							reject(response);
 						} else {
 							response = { status: "user_added" };
-							resolve(JSON.stringify(response));
+							resolve(response);
 						}
 					});
 				}
@@ -91,6 +67,34 @@ exports.getAllFlights = function (database, next) {
 					resolve(result);
 				} else {
 					reject(response);
+				}
+			}
+		});
+	});
+};
+
+exports.editUserInfo = function (database, next) {
+	return new Promise(async (resolve, reject) => {
+		let response = { status: "already_exists" };
+		let sql = `SELECT login FROM user WHERE login = '${data.login}'`;
+		database.query(sql, (err, result) => {
+			if (err) {
+				console.log("err in editUserInfo user");
+				reject("err");
+			} else {
+				if (result.length !== 0) {
+					resolve(JSON.stringify(response));
+				} else {
+					let addedSql = `insert into user (login, password, first_name, last_name, status, date_of_birth, address, sex) values ('${data.login}', '${data.password}', '${data.firstName}', '${data.lastName}', '${data.status}', '${data.dateOfBirth}', '${data.address}', '${data.sex}')`;
+					database.query(addedSql, (err, newResult) => {
+						if (err) {
+							console.log("err in register user");
+							reject("err");
+						} else {
+							response = { status: "user_added" };
+							resolve(JSON.stringify(response));
+						}
+					});
 				}
 			}
 		});
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,22 +1,6 @@
 module.exports = (server, database) => {
 	const requestsDB = require("../requests/requests");
 
-	server.post("/v1/signUp", getSignUp);
-
-	// function getSignUp(req, res, next) {
-	// 	const data = JSON.parse(req.body);
-	// 	if (!utils.isset(data.email, data.password)) {
-	// 		return next(new errs.InvalidArgumentError("Not enough body data"));
-	// 	}
-	// 	requestsDB.addUser(database, data, next)
-	// 		.then(() => {
-	// 			res.send("success");
-	// 		})
-	// 		.catch(() => {
-	// 			res.send("email is already exist");
-	// 		});
-	// }
-
 	server.post('/login', getSignIn);
 
 	function getSignIn(req, res, next) {
@@ -62,6 +46,26 @@ module.exports = (server, database) => {
 	function getFlights(req, res, next) {
 		console.log('flights');
 		requestsDB.getAllFlights(database, next)
+			.then((result) => {
+				console.log("responce: " + result);
+				res.send(JSON.stringify(result));
+			})
+			.catch((result) => {
+				console.log("reject: " + result);
+				res.send(JSON.stringify(result));
+			});
+	}
+
+	server.get('/user/change-info', changeUserInfo);
+
+	function changeUserInfo(req, res, next) {
+		console.log('changeUserInfo');
+		const data = req.body;
+		if (!req.body) {
+			res.send("error no body");
+		}
+
+		requestsDB.registerUser(database, data, next)
 			.then((result) => {
 				console.log("responce: " + result);
 				res.send(JSON.stringify(result));
