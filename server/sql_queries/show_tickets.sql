eac8abfe7d1da8be836fd430880891e958056cbb
Ilya Protasov <ilya.protasov@omega-r.com>
2018-10-31 22:44:58 +0300

    repair bugs

--- server/sql_queries/show_tickets.sql
+++ server/sql_queries/show_tickets.sql
@@ -17,3 +17,7 @@ INSERT INTO ticket (id_flight, id_class, price, decription) VALUES (1, 1, 100, '
 INSERT INTO ticket (id_flight, id_class, price, decription) VALUES (2, 2, 200, 'ticket2');
 INSERT INTO ticket (id_flight, id_class, price, decription) VALUES (2, 1, 300, 'ticket3');
 INSERT INTO ticket (id_flight, id_class, price, decription) VALUES (1, 2, 400, 'ticket4');
+
+
+
+SELECT ticket.id_ticket AS idTicket, ticket.price AS price, class.name AS name, class.description AS classDescription, ticket.decription AS ticketDescription FROM ticket LEFT JOIN class ON class.id_class = ticket.id_class WHERE ticket.id_flight = 1;
