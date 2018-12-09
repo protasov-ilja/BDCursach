a172847f27fb20d2b7d0ca06a0d9512a19bf3936
Ilya Protasov <ilya.protasov@omega-r.com>
2018-12-09 22:52:55 +0300

    added single ticket

--- server/requests/add_tickets.js
+++ server/requests/add_tickets.js
@@ -3,8 +3,8 @@ exports.addTickets = function (database, data, next) {
         console.log("addFlights");
         let response = { status: "added" };
         let sql = `
-            INSERT INTO ticket (id_flight, id_class, price, decription, place_number)
-            VALUES (?, ?, ?, ?, ?)`;
+          INSERT INTO ticket (id_flight, id_class, price, decription, place_number)
+          VALUES (?, ?, ?, ?, ?)`;
 
         for (let ticket of data.tickets) {
             console.log(ticket.idFlight, ticket.idClass, ticket.price, ticket.description, ticket.placeNumber);
@@ -15,6 +15,27 @@ exports.addTickets = function (database, data, next) {
                 }
             });
         }
+
+        resolve(response);
+    });
+};
+
+
+exports.addTicket = function (database, data, next) {
+    return new Promise(async (resolve, reject) => {
+        console.log("addTicket");
+        let response = { status: "added" };
+        let sql = `
+            INSERT INTO ticket (id_flight, id_class, price, decription, place_number)
+            VALUES (?, ?, ?, ?, ?)`;
+
+            // console.log(ticket.idFlight, ticket.idClass, ticket.price, ticket.description, ticket.placeNumber);
+            database.query(sql, [data.idFlight, data.idClass, data.price, data.description, data.placeNumber], (err, result) => {
+                if (err) {
+                    response = { status: "err in query" };
+                    reject(response);
+                }
+            });
 
         resolve(response);
     });
