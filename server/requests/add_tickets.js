a7c4a4d3b17e6b164f47c58e8c52a4df07b0d333
Ilya Protasov <ilya.protasov@omega-r.com>
2018-12-09 23:00:51 +0300

    added single ticket route

--- server/requests/add_tickets.js
+++ server/requests/add_tickets.js
@@ -29,13 +29,13 @@ exports.addTicket = function (database, data, next) {
             INSERT INTO ticket (id_flight, id_class, price, decription, place_number)
             VALUES (?, ?, ?, ?, ?)`;
 
-            // console.log(ticket.idFlight, ticket.idClass, ticket.price, ticket.description, ticket.placeNumber);
-            database.query(sql, [data.idFlight, data.idClass, data.price, data.description, data.placeNumber], (err, result) => {
-                if (err) {
-                    response = { status: "err in query" };
-                    reject(response);
-                }
-            });
+        // console.log(ticket.idFlight, ticket.idClass, ticket.price, ticket.description, ticket.placeNumber);
+        database.query(sql, [data.idFlight, data.idClass, data.price, data.description, data.placeNumber], (err, result) => {
+            if (err) {
+                response = { status: "err in query" };
+                reject(response);
+            }
+        });
 
         resolve(response);
     });
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -18,10 +18,12 @@ module.exports = (server, database) => {
     server.get('/flight/tickets', getTicketsForFlight); // + next TODO показ билетов, которые еще не забронированы
     server.post('/book-tickets', postBookTickets); // TODO !waiting for build!
     server.post('/admin/add-tickets', postAddTickets); // TODO !waiting for build!
+    server.post('/admin/add-ticket', postAddTicket); // TODO !waiting for build!
 	server.post('/admin/add-flight', postAddFlight); // TODO !waiting for build!
     server.post('/admin/add-flights', postAddFlights); // TODO !waiting for build!
 	// server.post('/flight/confirm', postConfirmBooking); // TODO
 	server.get('search-flights-by-city', getSearchFlights); // TODO !waiting for build!
+
 
     function getSignIn(req, res, next) {
         const data = req.body;
@@ -249,6 +251,24 @@ module.exports = (server, database) => {
         }
 
         addingTickets.addTickets(database, data, next)
+            .then((result) => {
+                console.log("response");
+                res.send(JSON.stringify(result));
+            })
+            .catch((error) => {
+                console.log("reject: " + error);
+                let response = {status: "error"};
+                res.send(JSON.stringify(response));
+            });
+    }
+
+    function postAddTicket(req, res, next) {
+        const data = req.body;
+        if (!req.body) {
+            res.send("error no body");
+        }
+
+        addingTickets.addTicket(database, data, next)
             .then((result) => {
                 console.log("response");
                 res.send(JSON.stringify(result));
