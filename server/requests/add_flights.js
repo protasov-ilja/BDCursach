700a1720898da8e9de8e3abf7a77b2edc5637995
Ilya Protasov <ilya.protasov@omega-r.com>
2018-12-09 23:08:15 +0300

    fix lexic error

--- server/requests/add_flights.js
+++ server/requests/add_flights.js
@@ -7,7 +7,7 @@ exports.addFlights = function (database, data, next) {
             INSERT INTO flight (id_plane, id_airport, point_of_departure, point_of_destination, time_of_departure, time_of_destination)
             VALUES (?, ?, ?, ?, ?, ?)`;
         for (let flight of data.flights) {
-            database.query(sql, [flight.idPlane, flight.idAirprot, flight.pointOfDeparture, flight.pointOfDestination, flight.timeOfDeparture, flight.timeOfDestination], (err, result) => {
+            database.query(sql, [flight.idPlane, flight.idAirport, flight.pointOfDeparture, flight.pointOfDestination, flight.timeOfDeparture, flight.timeOfDestination], (err, result) => {
                 if (err) {
                     response = { status: "err in query" };
                     reject(response);
@@ -23,11 +23,11 @@ exports.addFlight = function (database, data, next) {
     return new Promise(async (resolve, reject) => {
         console.log("addFlight");
         let response = { status: "added" };
-        console.log(data.idPlane, data.idAirprot, data.pointOfDeparture, data.pointOfDestination, data.timeOfDeparture, data.timeOfDestination);
+        console.log(data.idPlane, data.idAirport, data.pointOfDeparture, data.pointOfDestination, data.timeOfDeparture, data.timeOfDestination);
         let sql = `
             INSERT INTO flight (id_plane, id_airport, point_of_departure, point_of_destination, time_of_departure, time_of_destination)
             VALUES (?, ?, ?, ?, ?, ?)`;
-            database.query(sql, [data.idPlane, data.idAirprot, data.pointOfDeparture, data.pointOfDestination, data.timeOfDeparture, data.timeOfDestination], (err, result) => {
+            database.query(sql, [data.idPlane, data.idAirport, data.pointOfDeparture, data.pointOfDestination, data.timeOfDeparture, data.timeOfDestination], (err, result) => {
                 if (err) {
                     response = { status: "err in query" };
                     reject(response);
