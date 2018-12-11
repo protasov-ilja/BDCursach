06c3215885ce8336ed3e48ab7880005ed8ff28e4
Ilya Protasov <ilya.protasov@omega-r.com>
2018-12-11 23:03:23 +0300

    added company name in search

--- server/requests/search_flights.js
+++ server/requests/search_flights.js
@@ -2,15 +2,18 @@ exports.searchFlights = function(database, data, next) {
     return new Promise(async (resolve, reject) => {
         let response = { status: "no_flights" };
         let sql = `
-		SELECT
-			id_flight AS idFlight
-			, id_plane AS idPlane
-			, id_airport AS idAirport
-			, point_of_departure AS pointOfDeparture
-			, point_of_destination AS pointOfDestination
-			, time_of_departure AS timeOfDeparture
-			, time_of_destination AS timeOfDestination
-		FROM flight
+          SELECT
+                 id_flight AS idFlight
+              , id_plane AS idPlane
+              , id_airport AS idAirport
+              , point_of_departure AS pointOfDeparture
+              , point_of_destination AS pointOfDestination
+              , time_of_departure AS timeOfDeparture
+              , time_of_destination AS timeOfDestination
+              , company.name AS companyName
+          FROM flight
+            LEFT JOIN plane ON plane.id_plane = flight.id_plane
+            LEFT JOIN company ON company.id_company = plane.id_company
 			WHERE point_of_departure LIKE ? AND point_of_destination LIKE ?`;
 
         let pointOfDepartureSearched = data.pointOfDeparture + "%";
@@ -43,7 +46,10 @@ exports.searchFlightsByCity = function(database, data, next) {
 			, point_of_destination AS pointOfDestination
 			, time_of_departure AS timeOfDeparture
 			, time_of_destination AS timeOfDestination
+            , company.name AS companyName
 		FROM flight
+            LEFT JOIN plane ON plane.id_plane = flight.id_plane
+            LEFT JOIN company ON company.id_company = plane.id_company
 			WHERE point_of_departure LIKE ? OR point_of_destination LIKE ?`;
 
         let pointOfDepartureSearched = data.city + "%";
