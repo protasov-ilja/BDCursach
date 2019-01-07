a639bdf7f6452107d24e098a40d1a838c25f2536
Ilya Protasov <ilya.protasov@omega-r.com>
2019-01-08 00:32:05 +0300

    fix status

--- server/requests/post_confirm_booking.js
+++ server/requests/post_confirm_booking.js
@@ -1,17 +1,18 @@
 exports.confirmBooking = function (database, data, next) {
     return new Promise(async (resolve, reject) => {
         console.log("confirmBooking");
+        let response = { status: "payed" };
         let sql = `
 		UPDATE booking
 			SET status = 'payed'
 		    WHERE id_booking = ?`;
         database.query(sql, [data.idBooking], (err, result) => {
             if (err) {
-                let response = { status: "err in query" };
+                response = { status: "err in query" };
                 reject(response);
             }
 
-            resolve("payed");
+            resolve(response);
         });
     });
 };
