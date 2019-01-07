a6da9d095df7077ae1f48010d86706c67d58b1aa
Ilya Protasov <ilya.protasov@omega-r.com>
2019-01-07 22:35:19 +0300

    test flight

--- server/requests/get_user_booking.js
+++ server/requests/get_user_booking.js
@@ -1,18 +1,19 @@
 exports.bookingForUser = function(database, data, next) {
     return new Promise(async (resolve, reject) => {
-        console.log("checkUserAccess");
+        console.log("bookingForUser");
         let sql = `
 		        booking.id_booking AS idBooking
               , booking.status AS status
               , booking.number_card AS cardNumber
               , booking.date AS date
-              , SUM(ticket_in_booking.price) AS bookingPrice
+             
           FROM booking
                 LEFT JOIN 'user' USING (id_user)
-                LEFT JOIN ticket_in_booking USING (id_booking)
+                
             WHERE login = ? AND password = ?
-            GROUP BY id_booking`;
-
+            `;
+        // GROUP BY id_booking
+        // LEFT JOIN ticket_in_booking USING (id_booking)  , SUM(ticket_in_booking.price) AS bookingPrice
         database.query(sql, [data.login, data.password], (err, result) => {
             if (err) {
                 let response = "err in query";
