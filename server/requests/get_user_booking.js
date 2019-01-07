2ea61d02dc24dc30de8d5e9ab9a91440343b2d5b
Ilya Protasov <ilya.protasov@omega-r.com>
2019-01-07 22:40:24 +0300

    test flkight after fixing bug

--- server/requests/get_user_booking.js
+++ server/requests/get_user_booking.js
@@ -2,18 +2,18 @@ exports.bookingForUser = function(database, data, next) {
     return new Promise(async (resolve, reject) => {
         console.log("bookingForUser");
         let sql = `
+        SELECT
 		        booking.id_booking AS idBooking
               , booking.status AS status
               , booking.number_card AS cardNumber
               , booking.date AS date
-             
+              , SUM(ticket_in_booking.price) AS bookingPrice
           FROM booking
-                LEFT JOIN user USING (id_user)
-                
+                LEFT JOIN 'user' USING (id_user)
+                LEFT JOIN ticket_in_booking USING (id_booking)
             WHERE login = ? AND password = ?
-            `;
-        // GROUP BY id_booking
-        // LEFT JOIN ticket_in_booking USING (id_booking)  , SUM(ticket_in_booking.price) AS bookingPrice
+            GROUP BY id_booking`;
+
         database.query(sql, [data.login, data.password], (err, result) => {
             if (err) {
                 let response = "err in query";
