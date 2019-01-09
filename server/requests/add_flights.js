13df6bfb406fbda40522a4e98cdba2857f01b83a
Ilya Protasov <ilya.protasov@omega-r.com>
2019-01-10 01:18:06 +0300

    test flight

--- server/requests/add_flights.js
+++ server/requests/add_flights.js
@@ -28,17 +28,14 @@ exports.addFlight = function (database, data, next) {
         let sql = `
             INSERT INTO flight (id_plane, id_airport, point_of_departure, point_of_destination, time_of_departure, time_of_destination)
             VALUES (?, ?, ?, ?, ?, ?)`;
-        database.query(sql, [data.idPlane, data.idAirport, data.pointOfDeparture, data.pointOfDestination, new Date(data.timeOfDeparture), new Date(data.timeOfDestination)], (err, result) => {
+        await database.query(sql, [data.idPlane, data.idAirport, data.pointOfDeparture, data.pointOfDestination, new Date(data.timeOfDeparture), new Date(data.timeOfDestination)], (err, result) => {
             if (err) {
                 response = { status: "err in query" };
                 reject(response);
             }
-            console.log("First-response");
-            resolve(response);
-        });
 
-        console.log("Second-response");
-        response = { status: "error" };
-        resolve(response);
+            console.log("First-response");
+            return resolve(response);
+        });
     });
 };
